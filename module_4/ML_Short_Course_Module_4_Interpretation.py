86a517756107e7fd76f36e7445f7959226bd3d3b
thunderhoser <ryan.lagerquist@ou.edu>
2018-12-18 17:58:18 -0600

    Added IO methods for Module 4 (interpretation and visualization).

---
+++ module_4/ML_Short_Course_Module_4_Interpretation.py
@@ -0,0 +1,385 @@
+"""Code for AMS 2019 short course."""
+
+import copy
+import glob
+import time
+import calendar
+import os.path
+import numpy
+import pandas
+import netCDF4
+
+SEPARATOR_STRING = '\n\n' + '*' * 50 + '\n\n'
+
+DATE_FORMAT = '%Y%m%d'
+DATE_FORMAT_REGEX = '[0-9][0-9][0-9][0-9][0-1][0-9][0-3][0-9]'
+
+DEFAULT_IMAGE_DIR_NAME = (
+    '/home/ryan.lagerquist/Downloads/ams2019_short_course/'
+    'track_data_ncar_ams_3km_nc_small')
+DEFAULT_FEATURE_DIR_NAME = (
+    '/home/ryan.lagerquist/Downloads/ams2019_short_course/'
+    'track_data_ncar_ams_3km_csv_small')
+
+CSV_METADATA_COLUMNS = [
+    'Step_ID', 'Track_ID', 'Ensemble_Name', 'Ensemble_Member', 'Run_Date',
+    'Valid_Date', 'Forecast_Hour', 'Valid_Hour_UTC'
+]
+
+CSV_EXTRANEOUS_COLUMNS = [
+    'Duration', 'Centroid_Lon', 'Centroid_Lat', 'Centroid_X', 'Centroid_Y',
+    'Storm_Motion_U', 'Storm_Motion_V', 'Matched', 'Max_Hail_Size',
+    'Num_Matches', 'Shape', 'Location', 'Scale'
+]
+
+CSV_TARGET_NAME = 'RVORT1_MAX-future_max'
+TARGET_NAME = 'max_future_vorticity_s01'
+
+NETCDF_REFL_NAME = 'REFL_COM_curr'
+NETCDF_TEMP_NAME = 'T2_curr'
+NETCDF_U_WIND_NAME = 'U10_curr'
+NETCDF_V_WIND_NAME = 'V10_curr'
+NETCDF_PREDICTOR_NAMES = [
+    NETCDF_REFL_NAME, NETCDF_TEMP_NAME, NETCDF_U_WIND_NAME, NETCDF_V_WIND_NAME
+]
+
+REFLECTIVITY_NAME = 'reflectivity_dbz'
+TEMPERATURE_NAME = 'temperature_kelvins'
+U_WIND_NAME = 'u_wind_m_s01'
+V_WIND_NAME = 'v_wind_m_s01'
+PREDICTOR_NAMES = [
+    REFLECTIVITY_NAME, TEMPERATURE_NAME, U_WIND_NAME, V_WIND_NAME
+]
+
+NETCDF_TRACK_ID_NAME = 'track_id'
+NETCDF_TRACK_STEP_NAME = 'track_step'
+NETCDF_TARGET_NAME = 'RVORT1_MAX_future'
+
+STORM_IDS_KEY = 'storm_ids'
+STORM_STEPS_KEY = 'storm_steps'
+PREDICTOR_NAMES_KEY = 'predictor_names'
+PREDICTOR_MATRIX_KEY = 'predictor_matrix'
+TARGET_NAME_KEY = 'target_name'
+TARGET_MATRIX_KEY = 'target_matrix'
+
+
+def _remove_future_data(predictor_table):
+    """Removes future data from predictors.
+
+    :param predictor_table: pandas DataFrame with predictor values.  Each row is
+        one storm object.
+    :return: predictor_table: Same but with fewer columns.
+    """
+
+    predictor_names = list(predictor_table)
+    columns_to_remove = [p for p in predictor_names if 'future' in p]
+
+    return predictor_table.drop(columns_to_remove, axis=1, inplace=False)
+
+
+def _feature_file_name_to_date(csv_file_name):
+    """Parses date from name of feature (CSV) file.
+
+    :param csv_file_name: Path to input file.
+    :return: date_string: Date (format "yyyymmdd").
+    """
+
+    pathless_file_name = os.path.split(csv_file_name)[-1]
+    date_string = pathless_file_name.replace(
+        'track_step_NCARSTORM_d01_', '').replace('-0000.csv', '')
+
+    # Verify.
+    time_string_to_unix(time_string=date_string, time_format=DATE_FORMAT)
+    return date_string
+
+
+def _image_file_name_to_date(netcdf_file_name):
+    """Parses date from name of image (NetCDF) file.
+
+    :param netcdf_file_name: Path to input file.
+    :return: date_string: Date (format "yyyymmdd").
+    """
+
+    pathless_file_name = os.path.split(netcdf_file_name)[-1]
+    date_string = pathless_file_name.replace(
+        'NCARSTORM_', '').replace('-0000_d01_model_patches.nc', '')
+
+    # Verify.
+    time_string_to_unix(time_string=date_string, time_format=DATE_FORMAT)
+    return date_string
+
+
+def time_string_to_unix(time_string, time_format):
+    """Converts time from string to Unix format.
+
+    Unix format = seconds since 0000 UTC 1 Jan 1970.
+
+    :param time_string: Time string.
+    :param time_format: Format of time string (example: "%Y%m%d" or
+        "%Y-%m-%d-%H%M%S").
+    :return: unix_time_sec: Time in Unix format.
+    """
+
+    return calendar.timegm(time.strptime(time_string, time_format))
+
+
+def time_unix_to_string(unix_time_sec, time_format):
+    """Converts time from Unix format to string.
+
+    Unix format = seconds since 0000 UTC 1 Jan 1970.
+
+    :param unix_time_sec: Time in Unix format.
+    :param time_format: Desired format of time string (example: "%Y%m%d" or
+        "%Y-%m-%d-%H%M%S").
+    :return: time_string: Time string.
+    """
+
+    return time.strftime(time_format, time.gmtime(unix_time_sec))
+
+
+def find_many_feature_files(first_date_string, last_date_string,
+                            feature_dir_name=DEFAULT_FEATURE_DIR_NAME):
+    """Finds feature files in the given date range.
+
+    :param first_date_string: First date ("yyyymmdd") in range.
+    :param last_date_string: Last date ("yyyymmdd") in range.
+    :param feature_dir_name: Name of directory with feature (CSV) files.
+    :return: csv_file_names: 1-D list of paths to feature files.
+    """
+
+    first_time_unix_sec = time_string_to_unix(
+        time_string=first_date_string, time_format=DATE_FORMAT)
+    last_time_unix_sec = time_string_to_unix(
+        time_string=last_date_string, time_format=DATE_FORMAT)
+
+    csv_file_pattern = '{0:s}/track_step_NCARSTORM_d01_{1:s}-0000.csv'.format(
+        feature_dir_name, DATE_FORMAT_REGEX)
+    csv_file_names = glob.glob(csv_file_pattern)
+    csv_file_names.sort()
+
+    file_date_strings = [_feature_file_name_to_date(f) for f in csv_file_names]
+    file_times_unix_sec = numpy.array([
+        time_string_to_unix(time_string=d, time_format=DATE_FORMAT)
+        for d in file_date_strings
+    ], dtype=int)
+
+    good_indices = numpy.where(numpy.logical_and(
+        file_times_unix_sec >= first_time_unix_sec,
+        file_times_unix_sec <= last_time_unix_sec
+    ))[0]
+
+    return [csv_file_names[k] for k in good_indices]
+
+
+def read_feature_file(csv_file_name):
+    """Reads features from CSV file.
+
+    :param csv_file_name: Path to input file.
+    :return: metadata_table: pandas DataFrame with metadata.  Each row is one
+        storm object.
+    :return: predictor_table: pandas DataFrame with predictor values.  Each row
+        is one storm object.
+    :return: target_table: pandas DataFrame with target values.  Each row is one
+        storm object.
+    """
+
+    predictor_table = pandas.read_csv(csv_file_name, header=0, sep=',')
+    predictor_table.drop(CSV_EXTRANEOUS_COLUMNS, axis=1, inplace=True)
+
+    metadata_table = predictor_table[CSV_METADATA_COLUMNS]
+    predictor_table.drop(CSV_METADATA_COLUMNS, axis=1, inplace=True)
+
+    target_table = predictor_table[[CSV_TARGET_NAME]]
+    predictor_table.drop([CSV_TARGET_NAME], axis=1, inplace=True)
+    predictor_table = _remove_future_data(predictor_table)
+
+    return metadata_table, predictor_table, target_table
+
+
+def read_many_feature_files(csv_file_names):
+    """Reads features from many CSV files.
+
+    :param csv_file_names: 1-D list of paths to input files.
+    :return: metadata_table: See doc for `read_feature_file`.
+    :return: predictor_table: Same.
+    :return: target_table: Same.
+    """
+
+    num_files = len(csv_file_names)
+    list_of_metadata_tables = [pandas.DataFrame()] * num_files
+    list_of_predictor_tables = [pandas.DataFrame()] * num_files
+    list_of_target_tables = [pandas.DataFrame()] * num_files
+
+    for i in range(num_files):
+        print 'Reading data from: "{0:s}"...'.format(csv_file_names[i])
+
+        (list_of_metadata_tables[i], list_of_predictor_tables[i],
+         list_of_target_tables[i]
+        ) = read_feature_file(csv_file_names[i])
+
+        if i == 0:
+            continue
+
+        list_of_metadata_tables[i] = list_of_metadata_tables[i].align(
+            list_of_metadata_tables[0], axis=1
+        )[0]
+
+        list_of_predictor_tables[i] = list_of_predictor_tables[i].align(
+            list_of_predictor_tables[0], axis=1
+        )[0]
+
+        list_of_target_tables[i] = list_of_target_tables[i].align(
+            list_of_target_tables[0], axis=1
+        )[0]
+
+    metadata_table = pandas.concat(
+        list_of_metadata_tables, axis=0, ignore_index=True)
+    predictor_table = pandas.concat(
+        list_of_predictor_tables, axis=0, ignore_index=True)
+    target_table = pandas.concat(
+        list_of_target_tables, axis=0, ignore_index=True)
+
+    return metadata_table, predictor_table, target_table
+
+
+def find_many_image_files(first_date_string, last_date_string,
+                          image_dir_name=DEFAULT_IMAGE_DIR_NAME):
+    """Finds image (NetCDF) files in the given date range.
+
+    :param first_date_string: First date ("yyyymmdd") in range.
+    :param last_date_string: Last date ("yyyymmdd") in range.
+    :param image_dir_name: Name of directory with image (NetCDF) files.
+    :return: netcdf_file_names: 1-D list of paths to image files.
+    """
+
+    first_time_unix_sec = time_string_to_unix(
+        time_string=first_date_string, time_format=DATE_FORMAT)
+    last_time_unix_sec = time_string_to_unix(
+        time_string=last_date_string, time_format=DATE_FORMAT)
+
+    netcdf_file_pattern = (
+        '{0:s}/NCARSTORM_{1:s}-0000_d01_model_patches.nc'
+    ).format(image_dir_name, DATE_FORMAT_REGEX)
+
+    netcdf_file_names = glob.glob(netcdf_file_pattern)
+    netcdf_file_names.sort()
+
+    file_date_strings = [_image_file_name_to_date(f) for f in netcdf_file_names]
+    file_times_unix_sec = numpy.array([
+        time_string_to_unix(time_string=d, time_format=DATE_FORMAT)
+        for d in file_date_strings
+    ], dtype=int)
+
+    good_indices = numpy.where(numpy.logical_and(
+        file_times_unix_sec >= first_time_unix_sec,
+        file_times_unix_sec <= last_time_unix_sec
+    ))[0]
+
+    return [netcdf_file_names[k] for k in good_indices]
+
+
+def read_image_file(netcdf_file_name):
+    """Reads storm-centered images from NetCDF file.
+
+    E = number of examples (storm objects) in file
+    M = number of rows in each storm-centered grid
+    N = number of columns in each storm-centered grid
+    C = number of channels (predictor variables)
+
+    :param netcdf_file_name: Path to input file.
+    :return: image_dict: Dictionary with the following keys.
+    image_dict['storm_ids']: length-E list of storm IDs (integers).
+    image_dict['storm_steps']: length-E numpy array of storm steps (integers).
+    image_dict['predictor_names']: length-C list of predictor names.
+    image_dict['predictor_matrix']: E-by-M-by-N-by-C numpy array of predictor
+        values.
+    image_dict['target_name']: Name of target variable.
+    image_dict['target_matrix']: E-by-M-by-N numpy array of target values.
+    """
+
+    dataset_object = netCDF4.Dataset(netcdf_file_name)
+
+    storm_ids = numpy.array(
+        dataset_object.variables[NETCDF_TRACK_ID_NAME][:], dtype=int)
+    storm_steps = numpy.array(
+        dataset_object.variables[NETCDF_TRACK_STEP_NAME][:], dtype=int)
+
+    predictor_matrix = None
+
+    for this_predictor_name in NETCDF_PREDICTOR_NAMES:
+        this_predictor_matrix = numpy.array(
+            dataset_object.variables[this_predictor_name][:], dtype=float)
+        this_predictor_matrix = numpy.expand_dims(
+            this_predictor_matrix, axis=-1)
+
+        if predictor_matrix is None:
+            predictor_matrix = this_predictor_matrix + 0.
+        else:
+            predictor_matrix = numpy.concatenate(
+                (predictor_matrix, this_predictor_matrix), axis=-1)
+
+    target_matrix = numpy.array(
+        dataset_object.variables[NETCDF_TARGET_NAME][:], dtype=float)
+
+    return {
+        STORM_IDS_KEY: storm_ids,
+        STORM_STEPS_KEY: storm_steps,
+        PREDICTOR_NAMES_KEY: PREDICTOR_NAMES,
+        PREDICTOR_MATRIX_KEY: predictor_matrix,
+        TARGET_NAME_KEY: TARGET_NAME,
+        TARGET_MATRIX_KEY: target_matrix
+    }
+
+
+def read_many_image_files(netcdf_file_names):
+    """Reads storm-centered images from many NetCDF files.
+
+    :param netcdf_file_names: 1-D list of paths to input files.
+    :return: image_dict: See doc for `read_image_file`.
+    """
+
+    image_dict = None
+    keys_to_concat = [
+        STORM_IDS_KEY, STORM_STEPS_KEY, PREDICTOR_MATRIX_KEY, TARGET_MATRIX_KEY
+    ]
+
+    for this_file_name in netcdf_file_names:
+        print 'Reading data from: "{0:s}"...'.format(this_file_name)
+        this_image_dict = read_image_file(this_file_name)
+
+        if image_dict is None:
+            image_dict = copy.deepcopy(this_image_dict)
+            continue
+
+        for this_key in keys_to_concat:
+            image_dict[this_key] = numpy.concatenate(
+                (image_dict[this_key], this_image_dict[this_key]), axis=0)
+
+    return image_dict
+
+
+def _run():
+    """This is effectively the main method for now."""
+
+    csv_file_names = find_many_feature_files(
+        first_date_string='20150101', last_date_string='20151231')
+    metadata_table, predictor_table, target_table = read_many_feature_files(
+        csv_file_names)
+
+    print SEPARATOR_STRING
+    print 'Number of storm objects in feature dataset: {0:d}'.format(
+        len(metadata_table.index))
+    print SEPARATOR_STRING
+
+    netcdf_file_names = find_many_image_files(
+        first_date_string='20150101', last_date_string='20151231')
+    image_dict = read_many_image_files(netcdf_file_names)
+
+    print SEPARATOR_STRING
+    print 'Shape of predictor matrix: {0:s}'.format(
+        str(image_dict[PREDICTOR_MATRIX_KEY].shape)
+    )
+
+
+if __name__ == '__main__':
+    _run()
